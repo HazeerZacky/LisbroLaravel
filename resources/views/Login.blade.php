75ee7e9ac1a8867664268c9912383174cc655d07
Hazeerzacky <contact.hazky@gmail.com>
2022-04-18 20:37:14 +0530

    2

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -1,24 +1,30 @@
+<!doctype html>
 <html lang="en">
 
 <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <title>Login</title>
+    <title>Test</title>
 </head>
 
 <body>
-<div class="card text-center">
-  <div class="card-header">
-    Featured
-  </div>
-  <div class="card-body">
-    <h5 class="card-title">Special title treatment</h5>
-    <p class="card-text">With supporting text below as a natural lead-in to additional content.</p>
-    <a href="#" class="btn btn-primary">Go somewhere</a>
-  </div>
-  <div class="card-footer text-muted">
-    2 days ago
-  </div>
-</div>
+    <div class="text-center mt-5">
+        <form style="max-width:480px; margin:auto;">
+            <h1 class="mt-4">Please sign in</h1>
+            <input type="email" name="emailAddress" id="emailAddress" class="form-control mt-3" placeholder="Email Address" required autofocus>
+            <input type="password" name="password" id="password" class="form-control mt-3" placeholder="Password" required>
+            <div class="d-grid gap-2 mt-3">
+                <button class="btn btn-primary" type="button">Sign In</button>
+            </div>
+        </form>
+    </div>
+
+    <!-- Bootstrap JS -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
 </body>
 
 </html>
--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -12,16 +12,99 @@
 </head>
 
 <body>
-    <div class="text-center mt-5">
-        <form style="max-width:480px; margin:auto;">
-            <h1 class="mt-4">Please sign in</h1>
-            <input type="email" name="emailAddress" id="emailAddress" class="form-control mt-3" placeholder="Email Address" required autofocus>
-            <input type="password" name="password" id="password" class="form-control mt-3" placeholder="Password" required>
-            <div class="d-grid gap-2 mt-3">
-                <button class="btn btn-primary" type="button">Sign In</button>
+
+  <!-- Model Start   -->
+  <!-- Add Model Start -->
+  <div class="modal fade" id="AddClass" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+            <div class="modal-header">
+                <h5 class="modal-title" id="exampleModalLabel">&#9776; Class Form</h5>
+                <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                <span aria-hidden="true">&times;</span>
+                </button>
             </div>
-        </form>
-    </div>
+            <div class="modal-body">
+            <!-- form start -->
+                    <form role="form" action="/addclass" method="post">
+                    @csrf
+                        <div class="form-group">
+                            <label for="exampleInputText" class="form-label">Class Name</label>
+                            <input type="text" class="form-control"name="CName" placeholder="Enter class name">
+                        </div>
+                        <div class="form-group">
+                        <label>Class Type</label>
+                            <select class="form-control select2" name="CType" data-placeholder="Select an option">
+                                <option value="" selected disabled hidden>(select an option)</option>
+                                <option value="GCE-A/L"><b>GCE Advanced Level</b></option>
+                                <option value="GCE-O/L"><b>GCE Ordinary Level</b></option>
+                                <option value="SecondaryLevel"><b>Secondary Level</b></option>
+                                <option value="PrimaryLevel"><b>Primary Level</b></option>
+                            </select>
+                        </div>
+                        <div class="form-group">
+                            <label for="exampleInputText" class="form-label">Status</label><br>
+                            <div class="custom-control custom-radio custom-control-inline">
+                              <input type="radio" id="customRadioInline1" value="Active" name="CStatus" class="custom-control-input">
+                              <label class="custom-control-label" for="customRadioInline1">Active</label>
+                            </div>
+                            <div class="custom-control custom-radio custom-control-inline">
+                              <input type="radio" id="customRadioInline2" value="Deactive" name="CStatus" class="custom-control-input">
+                              <label class="custom-control-label" for="customRadioInline2">Deactive</label>
+                            </div>
+                        </div>
+                        </div>
+                                <div class="modal-footer">
+                                    <button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+                                    <button type="submit" class="btn btn-primary ">Save changes</button>
+                                </div> 
+                            </div>
+                    </form>
+            
+        </div>
+        </div>
+  <!-- Add Model End -->
+
+
+
+<div class="card">
+              <!-- Table part start -->
+              <div class="card-body">
+                <!-- Add Button Part Start -->
+                <div class="row">
+                            <div class="col-md-12 text-end">
+                              <button type="button" class="btn btn-success" data-bs-toggle="modal" data-bs-target="#AddClass">Add New Class</button>
+                            </div>
+                </div>
+                <br>
+                <!-- Add Button Part End -->
+                <table id="example1" class="table table-bordered table-striped">
+                  <thead>
+                  <tr>
+                    <th scope="col">User ID</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Password</th>
+                    <th style="width:  12%">Action</th>
+                  </tr>
+                  </thead>
+                  <tbody>
+                    
+                  </tbody>
+                  <tfoot>
+                  <tr>
+                    <th scope="col">User ID</th>
+                    <th scope="col">Email</th>
+                    <th scope="col">Password</th>
+                    <th style="width:  12%">Action</th>
+                  </tr>
+                  </tfoot>
+                </table>
+              </div>
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+  </div>
+  
 
     <!-- Bootstrap JS -->
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
