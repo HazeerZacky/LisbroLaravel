6c3a393cef2e1b00b0ba0d6e419eefc065513ec7
Hazeerzacky <contact.hazky@gmail.com>
2022-04-21 08:37:18 +0530

    Alignment Login

--- resources/views/Login.blade.php
+++ resources/views/Login.blade.php
@@ -1,18 +1,14 @@
-<!doctype html>
-<html lang="en">
+<html>
 
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="{{asset('plugins')}}/bootstrap/css/bootstrap.min.css" >
     <script src="{{asset('plugins')}}/bootstrap/js/bootstrap.bundle.min.js" ></script>
-
     <!-- jquery -->
     <script src="{{asset('plugins')}}/jquery/jquery.min.js"></script>
-
     <!-- toastr -->
 	<link rel="stylesheet" type="text/css" href="{{asset('plugins')}}/toastr/toastr.min.css">
     <script src="{{asset('plugins')}}/toastr/toastr.min.js"></script>
@@ -33,39 +29,35 @@
         </form>
     </div>
 
-    <!-- Alert Part Start -->
 
-  @if(Session::has('message'))
-  <script>
+<!-- Alert Part Start -->
+    @if(Session::has('message'))
+    <script>
     var type = "{{ Session::get('alert-type', 'info') }}";
     switch(type){
         case 'info':
             toastr.info("{{ Session::get('message') }}");
             break;
-        
         case 'warning':
             toastr.warning("{{ Session::get('message') }}");
             break;
-
         case 'success':
             toastr.success("{{ Session::get('message') }}");
             break;
-
         case 'error':
             toastr.error("{{ Session::get('message') }}");
             break;
     }
     </script>
     @endif
-        @if($errors->any())
-          @foreach($errors->all()  as $error)
-          <script>
-            toastr.info("{{$error}}");
-          </script>
-          @endforeach
-        @endif
+
+    @if($errors->any())
+        @foreach($errors->all()  as $error)
+        <script>
+        toastr.info("{{$error}}");
+        </script>
+        @endforeach
+    @endif
 <!-- Alert Part End -->
-
 </body>
-
 </html>
